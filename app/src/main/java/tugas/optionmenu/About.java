57aee9acc3dc6fed733866e3ae4c7fd39c9e0227
yulpan347 <1406130@sttgarut.ac.id>
2017-12-03 22:59:47 +0700

    Tugas Option Menu Yulpan Paisal

---
+++ app/src/main/java/tugas/optionmenu/About.java
@@ -0,0 +1,28 @@
+package tugas.optionmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class About extends AppCompatActivity {
+    WebView browser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        browser=(WebView)findViewById(R.id.about_apps);
+        //Use same layout and manifest of previous example
+        browser.loadData("" +
+                "<html>" +
+                "   <body>" +
+                "  <title>About Aplikasi </title>" +
+                "<p>" +
+                "Aplikasi pencatatan waktu olahraga\n" +
+                "Aplikasi ini dibuat oleh :<br/>" +
+                "Rivan Mulyana (1406108)<br/>" +
+                "Yulpan Paisal (1406130)<br/>" +
+                "</p>" +
+                "</body>" +
+                "</html>", "text/html","UTF-8");
+    }
+}
